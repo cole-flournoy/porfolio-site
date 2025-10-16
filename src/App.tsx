996d804afa4ef4895636ebd41544a91cc5216492
Cole Flournoy <flournoy.cole@gmail.com>
2025-10-16 16:48:02 -0400

    Update icons and links

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import linkedInLogo from './assets/linkedin_icon.svg'
+import gitHubLogo from './assets/github_icon.svg'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <div>
-        {/* <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo" alt="React logo" />
-        </a> */}
-      </div>
       <h1>Cole Flournoy</h1>
-      <p>Portfolio site coming soon</p>
+      <p>Portfolio site coming soon!</p>
+      <div>
+        <a href="https://www.linkedin.com/in/cole-e-flournoy/" target="_blank">
+          <img src={linkedInLogo} className="logo" alt="LinkedIn logo" />
+        </a>
+        <a href="https://github.com/cole-flournoy" target="_blank">
+          <img src={gitHubLogo} className="logo" alt="GitHub logo" />
+        </a>
+      </div>
     </>
   )
 }
