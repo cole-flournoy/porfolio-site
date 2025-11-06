e4f8785ced78eaaa981324a057bc72cfb308d054
Cole Flournoy <flournoy.cole@gmail.com>
2025-11-06 13:18:46 -0500

    Reverse hover logic for readability

--- src/sections/Socials.tsx
+++ src/sections/Socials.tsx
@@ -6,7 +6,13 @@ import type { HoveredSection } from '../App'
 
 const Socials = ({hoveredSection}: { hoveredSection: HoveredSection }  ) => {
   
-  if (hoveredSection === 'socials') {
+  if (hoveredSection !== 'socials') {
+    return (
+      <span id='socials' style={{ color: 'var(--color-faded-light-text)', fontSize: 'var(--text-size-header)', textAlign: 'center', letterSpacing: '5px' }}>
+        SOCIALS
+      </span>
+    )
+  } else {
     return ( 
       <span id='socials' style={{ display: 'flex', flexDirection: 'row', gap: '30px', alignItems: 'center', justifyContent: 'center'  }}>
         <a href='https://www.linkedin.com/in/cole-e-flournoy/' target='_blank' style={{ display: 'flex' }}>
@@ -20,12 +26,6 @@ const Socials = ({hoveredSection}: { hoveredSection: HoveredSection }  ) => {
         </a>
       </span>
     );
-  } else {
-    return (
-      <span id='socials' style={{ color: 'var(--color-faded-light-text)', fontSize: 'var(--text-size-header)', textAlign: 'center', letterSpacing: '5px' }}>
-        SOCIALS
-      </span>
-    )
   }
 }
 
