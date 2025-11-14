416b0af0e97fe9d2c310fcd164dd5ba21bbceadc
Cole Flournoy <flournoy.cole@gmail.com>
2025-11-13 23:14:46 -0500

    Remove conditional rendering in education section

--- src/sections/Education.tsx
+++ src/sections/Education.tsx
@@ -1,20 +1,23 @@
 import GraduationIcon from '../assets/graduation_icon.svg?react'
 import CheckBoxIcon from '../assets/check_box_icon.svg?react'
 import type { HoveredSection } from '../App'
+import './styles/Education.css'
 
 const Education = ({hoveredSection}: { hoveredSection: HoveredSection }) => {
-  if (hoveredSection !== 'education') {
-    return (
-      <span id="education" style={{ display: 'flex', flexDirection: 'column', gap: '5px', alignItems: 'center', justifyContent: 'center' }}>
+  const isHovered = hoveredSection === 'education'
+  
+  return (
+    <span className={`education ${isHovered ? 'hovered' : ''}`} style={{ }}>
+      {/* Starting view - label */}
+      <span className='education-label' aria-hidden={isHovered ? 'true' : 'false'}>
         <GraduationIcon style={{ width: '100px', height: '100px', color: 'var(--color-faded-light-text)' }} />
         <span style={{ fontSize: 'var(--text-size-header)', textAlign: 'center', letterSpacing: '5px', color: 'var(--color-faded-light-text)' }}>
           EDUCATION
         </span>
       </span>
-    )
-  } else {
-    return (
-      <span id="education" style={{ display: 'flex', flexDirection: 'column', gap: '15px', alignItems: 'flex-start', justifyContent: 'center', padding: '10px'}}>
+
+      {/* Hovered view - checklist */}
+      <span className='education-checklist' aria-hidden={isHovered ? 'false' : 'true'}>
         <span style={{ display: 'flex', flexDirection: 'row', fontSize: 'var(--text-size-subheader)', color: 'var(--color-light-text)', gap: '15px' }}>
           <CheckBoxIcon style={{ width: '25px', height: '25px', color: 'var(--color-light-text)', flexShrink: '0'}} />
           <span>
@@ -28,8 +31,8 @@ const Education = ({hoveredSection}: { hoveredSection: HoveredSection }) => {
           </span>
         </span>
       </span>
-    )
-  }
+    </span>
+  )
 }
 
 export default Education
