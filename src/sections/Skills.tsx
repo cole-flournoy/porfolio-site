bb2594129c821171893a6e304f707b35c35287aa
Cole Flournoy <flournoy.cole@gmail.com>
2025-11-15 11:01:44 -0500

    Standardize code style

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -9,12 +9,12 @@ import VSCodeLogo from '../assets/tech_icons/vscode_logo.svg?react'
 import type { HoveredSection } from '../App'
 import { useState } from 'react'
 
-type SkillName = 'TypeScript' | 'React.js' | 'Node.js' | 'PostgreSQL' | 'Git' | 'Jira' | 'Figma' | 'VSCode' | null;
+type SkillName = 'TypeScript' | 'React.js' | 'Node.js' | 'PostgreSQL' | 'Git' | 'Jira' | 'Figma' | 'VSCode' | null
 
 const Skills = ({hoveredSection}: { hoveredSection: HoveredSection }) => {
   const isHovered = hoveredSection === 'skills'
   const textColor = isHovered ? 'var(--color-light-text)' : 'var(--color-faded-light-text)'
-  const [hoveredSkill, setHoveredSkill] = useState<SkillName>(null);
+  const [hoveredSkill, setHoveredSkill] = useState<SkillName>(null)
 
   const SkillIcon = ({ techName, iconComponent }: { techName: SkillName, iconComponent: React.JSX.Element }) => (
     <span style={{ position: 'relative', display: 'flex', flexDirection: 'column', alignItems: 'center' }} onMouseEnter={() => setHoveredSkill(techName)} onMouseLeave={() => setHoveredSkill(null)}>
@@ -24,12 +24,12 @@ const Skills = ({hoveredSection}: { hoveredSection: HoveredSection }) => {
   )
 
   return (
-    <span id="skills" style={{ position: 'relative', display: 'grid', gridTemplateColumns: '1fr 1fr', gridAutoFlow: 'column', gridTemplateRows: 'repeat(4, auto)', alignItems: 'center', justifyItems: 'center', rowGap: '25px', width: '100%', color: textColor }}>
+    <span className='skills' style={{ position: 'relative', display: 'grid', gridTemplateColumns: '1fr 1fr', gridAutoFlow: 'column', gridTemplateRows: 'repeat(4, auto)', alignItems: 'center', justifyItems: 'center', rowGap: '25px', width: '100%', color: textColor }}>
       {/* Column 1 */}
-      <SkillIcon techName="TypeScript" iconComponent={<TypescriptLogo className='icon' />} />
-      <SkillIcon techName="React.js" iconComponent={<ReactLogo className='icon' />} />
-      <SkillIcon techName="Node.js" iconComponent={<NodeLogo className='icon' />} />
-      <SkillIcon techName="PostgreSQL" iconComponent={<PostgresLogo className='icon' />} />
+      <SkillIcon techName='TypeScript' iconComponent={<TypescriptLogo className='icon' />} />
+      <SkillIcon techName='React.js' iconComponent={<ReactLogo className='icon' />} />
+      <SkillIcon techName='Node.js' iconComponent={<NodeLogo className='icon' />} />
+      <SkillIcon techName='PostgreSQL' iconComponent={<PostgresLogo className='icon' />} />
 
       <span style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', height: '75px', width: '1px', backgroundColor: textColor }}>
         <span style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', height: '7px', width: '7px', backgroundColor: 'var(--color-grid-background)', borderRadius: '50%', zIndex: 5 }}></span> 
@@ -37,10 +37,10 @@ const Skills = ({hoveredSection}: { hoveredSection: HoveredSection }) => {
       </span>      
 
       {/* Column 2 */}
-      <SkillIcon techName="Git" iconComponent={<GitLogo className='icon' />} />
-      <SkillIcon techName="Jira" iconComponent={<JiraLogo className='icon' />} />
-      <SkillIcon techName="Figma" iconComponent={<FigmaLogo className='icon' />} />
-      <SkillIcon techName="VSCode" iconComponent={<VSCodeLogo className='icon' />} />
+      <SkillIcon techName='Git' iconComponent={<GitLogo className='icon' />} />
+      <SkillIcon techName='Jira' iconComponent={<JiraLogo className='icon' />} />
+      <SkillIcon techName='Figma' iconComponent={<FigmaLogo className='icon' />} />
+      <SkillIcon techName='VSCode' iconComponent={<VSCodeLogo className='icon' />} />
     </span>
   )
 }
