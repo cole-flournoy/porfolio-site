8257b838aba7681d6e346aeae5be9101de14662c
Cole Flournoy <flournoy.cole@gmail.com>
2025-11-15 17:20:50 -0500

    Animate skills hover transition

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -8,30 +8,30 @@ import FigmaLogo from '../assets/tech_icons/figma_logo.svg?react'
 import VSCodeLogo from '../assets/tech_icons/vscode_logo.svg?react'
 import type { HoveredSection } from '../App'
 import { useState } from 'react'
+import './styles/Skills.css'
 
 type SkillName = 'TypeScript' | 'React.js' | 'Node.js' | 'PostgreSQL' | 'Git' | 'Jira' | 'Figma' | 'VSCode' | null
 
 const Skills = ({hoveredSection}: { hoveredSection: HoveredSection }) => {
   const isHovered = hoveredSection === 'skills'
-  const textColor = isHovered ? 'var(--color-light-text)' : 'var(--color-faded-light-text)'
   const [hoveredSkill, setHoveredSkill] = useState<SkillName>(null)
 
   const SkillIcon = ({ techName, iconComponent }: { techName: SkillName, iconComponent: React.JSX.Element }) => (
     <span style={{ position: 'relative', display: 'flex', flexDirection: 'column', alignItems: 'center' }} onMouseEnter={() => setHoveredSkill(techName)} onMouseLeave={() => setHoveredSkill(null)}>
       {iconComponent}
-      {(hoveredSkill === techName && isHovered) && <span style={{ color: textColor, fontSize: 'var(--text-size-small)', position: 'absolute', bottom: '-18px' }}>{techName}</span>}
+      {(hoveredSkill === techName && isHovered) && <span style={{ fontSize: 'var(--text-size-small)', position: 'absolute', bottom: '-18px', color: 'var(--color-light-text)' }}>{techName}</span>}
     </span>
   )
 
   return (
-    <span className='skills' style={{ position: 'relative', display: 'grid', gridTemplateColumns: '1fr 1fr', gridAutoFlow: 'column', gridTemplateRows: 'repeat(4, auto)', alignItems: 'center', justifyItems: 'center', rowGap: '25px', width: '100%', color: textColor }}>
+    <span className={`skills ${isHovered ? 'hovered' : ''}`} style={{ }}>
       {/* Column 1 */}
       <SkillIcon techName='TypeScript' iconComponent={<TypescriptLogo className='icon' />} />
       <SkillIcon techName='React.js' iconComponent={<ReactLogo className='icon' />} />
       <SkillIcon techName='Node.js' iconComponent={<NodeLogo className='icon' />} />
       <SkillIcon techName='PostgreSQL' iconComponent={<PostgresLogo className='icon' />} />
 
-      <span style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', height: '75px', width: '1px', backgroundColor: textColor }}>
+      <span className='divider' style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', height: '75px', width: '1px' }}>
         <span style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', height: '7px', width: '7px', backgroundColor: 'var(--color-grid-background)', borderRadius: '50%', zIndex: 5 }}></span> 
         <span style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', height: '3px', width: '3px', backgroundColor: 'inherit', borderRadius: '50%', zIndex: 10 }}></span> 
       </span>      
