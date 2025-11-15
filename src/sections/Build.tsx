0ad6e4ffac1147a7cd7454e48c8eeaf19bb8f66f
Cole Flournoy <flournoy.cole@gmail.com>
2025-11-15 10:51:58 -0500

    Convert build section CSS and add animation

--- src/sections/Build.tsx
+++ src/sections/Build.tsx
@@ -1,16 +1,20 @@
 import WebpagesIcon from '../assets/tech_icons/webpages_icon.svg?react'
 import type { HoveredSection } from '../App'
+import './styles/Build.css'
 
 
 const Build = ({hoveredSection}: { hoveredSection: HoveredSection }) => {
   const isHovered = hoveredSection === 'build';
-  const textColor = isHovered ? 'var(--color-light-text)' : 'var(--color-faded-light-text)'
   
   return (
-    <span id='build' style={{ display: 'flex', flexDirection: 'column', gap: '15px', alignItems: 'center', justifyContent: 'center', color: textColor, cursor: isHovered ? 'pointer' : 'default' }}>
-      {!isHovered && <WebpagesIcon style={{ width: '100px', height: '100px' }} />}
-      <span style={{ fontSize: 'var(--text-size-header)', textAlign: 'center', letterSpacing: '5px', lineHeight: '34px' }}>
-        {isHovered ? <a href='https://github.com/cole-flournoy/portfolio-site?tab=readme-ov-file#portfolio-website' target='_blank' style={{ textDecoration: 'none', color: textColor }}>CURIOUS ABOUT THIS SITE?</a> : 'SITE BUILD'}
+    <span className={`build ${isHovered ? 'hovered' : ''}`} >
+      <span className='build-label' aria-hidden={isHovered ? 'true' : 'false'}>
+        <WebpagesIcon style={{ width: '100px', height: '100px' }} />
+        SITE BUILD
+      </span>
+
+      <span className='build-detail' aria-hidden={isHovered ? 'false' : 'true'}>
+        CURIOUS ABOUT THIS SITE?
       </span>
     </span>
   )
