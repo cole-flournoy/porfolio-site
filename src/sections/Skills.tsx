467104a3cc81051c34070baacc7eeb00ae39d907
Cole Flournoy <flournoy.cole@gmail.com>
2025-11-28 16:11:41 -0500

    Style main grid sections to be mobile-friendly

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -15,12 +15,12 @@ type SkillName = 'TypeScript' | 'React.js' | 'Node.js' | 'PostgreSQL' | 'Git' | 
 const Skills = ({isHovered}: { isHovered: boolean }) => {
   const [hoveredSkill, setHoveredSkill] = useState<SkillName>(null)
 
-  const isConstrainedScreen = useMediaQuery({ query: '(max-width: 1111px)' })
+  const isConstrainedScreen = useMediaQuery({ query: '(max-width: 1111px, min-width: 850px)' })
 
   const SkillIcon = ({ techName, iconComponent }: { techName: SkillName, iconComponent: React.JSX.Element }) => (
     <span style={{ position: 'relative', display: 'flex', flexDirection: 'column', alignItems: 'center' }} onMouseEnter={() => setHoveredSkill(techName)} onMouseLeave={() => setHoveredSkill(null)}>
       {iconComponent}
-      {(hoveredSkill === techName && isHovered) && <span style={{ fontSize: 'var(--text-size-small)', position: 'absolute', bottom: '-18px', color: 'var(--color-light-text)' }}>{techName}</span>}
+      {(hoveredSkill === techName && isHovered && isConstrainedScreen) && <span style={{ fontSize: 'var(--text-size-small)', position: 'absolute', bottom: '-18px', color: 'var(--color-light-text)' }}>{techName}</span>}
     </span>
   )
 
