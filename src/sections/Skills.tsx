d6f623042026ea129756f926c6c1443e4ac3126a
Cole Flournoy <flournoy.cole@gmail.com>
2025-11-29 21:31:36 -0500

    Only show skill divider and labels on web

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -9,18 +9,21 @@ import VSCodeLogo from '../assets/tech_icons/vscode_logo.svg?react'
 import { useState } from 'react'
 import './styles/Skills.scss'
 import { useMediaQuery } from 'react-responsive'
+import * as bp from '../constants/breakpoints'
 
 type SkillName = 'TypeScript' | 'React.js' | 'Node.js' | 'PostgreSQL' | 'Git' | 'Jira' | 'Figma' | 'VSCode' | null
 
 const Skills = ({isHovered}: { isHovered: boolean }) => {
   const [hoveredSkill, setHoveredSkill] = useState<SkillName>(null)
 
-  const isConstrainedScreen = useMediaQuery({ query: '(min-width: 850px) and (max-width: 1111px)' })
+  const showDivider = useMediaQuery({ query: '(min-width: 1111px)' })
+  const showSkillLabels = useMediaQuery({ query: `(min-width: ${bp.MEDIUM_2}px)`})
+
 
   const SkillIcon = ({ techName, iconComponent }: { techName: SkillName, iconComponent: React.JSX.Element }) => (
     <span style={{ position: 'relative', display: 'flex', flexDirection: 'column', alignItems: 'center' }} onMouseEnter={() => setHoveredSkill(techName)} onMouseLeave={() => setHoveredSkill(null)}>
       {iconComponent}
-      {(hoveredSkill === techName && isHovered && isConstrainedScreen) && <span style={{ fontSize: 'var(--text-size-small)', position: 'absolute', bottom: '-18px', color: 'var(--color-light-text)' }}>{techName}</span>}
+      {(hoveredSkill === techName && isHovered && showSkillLabels) && <span style={{ fontSize: 'var(--text-size-small)', position: 'absolute', bottom: '-18px', color: 'var(--color-light-text)' }}>{techName}</span>}
     </span>
   )
 
@@ -32,12 +35,12 @@ const Skills = ({isHovered}: { isHovered: boolean }) => {
       <SkillIcon techName='Node.js' iconComponent={<NodeLogo className='icon' />} />
       <SkillIcon techName='PostgreSQL' iconComponent={<PostgresLogo className='icon' />} />
 
-    {!isConstrainedScreen && 
-      <span className='divider'>
-        <span className='outer-dot'></span> 
-        <span className='inner-dot'></span> 
-      </span>
-    }     
+      {showDivider && 
+        <span className='divider'>
+          <span className='outer-dot'></span> 
+          <span className='inner-dot'></span> 
+        </span>
+      }     
 
       {/* Column 2 */}
       <SkillIcon techName='Git' iconComponent={<GitLogo className='icon' />} />
